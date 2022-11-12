2ca9e3972cc5d09ffd10dc1a1ff3098c2e3e2b93
Apiwatskt <monza581+github@gmail.com>
2022-11-12 22:19:31 +0700

    add JS

---
+++ index.js
@@ -0,0 +1,13 @@
+const convertBtn = document.getElementById('convert-btn');
+const lengthDisplay = document.getElementById('lengthdisplay');
+const volumeDisplay = document.getElementById('volumedisplay');
+const massDisplay = document.getElementById('massdisplay');
+const numInput = document.getElementById('num-input');
+
+
+
+
+
+convertBtn.addEventListener('click', function() {
+    
+})
