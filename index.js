b1c5ef8e6889c7e7f06fa7220f816af386bb67fa
Apiwatskt <monza581+github@gmail.com>
2022-11-13 13:47:25 +0700

    finish the assignment

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const volumeDisplay = document.getElementById('volumedisplay');
 const massDisplay = document.getElementById('massdisplay');
 const numInput = document.getElementById('num-input');
 
-
-
-
+const lengthConversionValue = 3.2808
+const volumeConversionValue = 3.785411784
+const massConversionValue = 2.20462262
 
 convertBtn.addEventListener('click', function() {
-    
+    const inputValue = Number(numInput.value)
+    lengthDisplay.innerText = `${numInput.value} meters = ${(inputValue*lengthConversionValue).toFixed(3)} feet | ${numInput.value} feet = ${(inputValue/lengthConversionValue).toFixed(3)} meters`
+    volumeDisplay.innerText = `${numInput.value} liters = ${(inputValue/volumeConversionValue).toFixed(3)} gallons | ${numInput.value} gallons = ${(inputValue*volumeConversionValue).toFixed(3)} liters`
+    massDisplay.innerText = `${numInput.value} kilos = ${(inputValue*massConversionValue).toFixed(3)} pounds | ${numInput.value} pounds = ${(inputValue/massConversionValue).toFixed(3)} kilos`
 })
